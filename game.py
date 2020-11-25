119379f583efa1064b5fe6443e53a6b63023f2b8
Someone-1243 <justinhye@gmail.com>
2020-11-25 09:21:33 -0500

    main class

--- game.py
+++ game.py
@@ -1,19 +1,8 @@
 import pygame
 
-pygame.init()
-
-clock = pygame.time.Clock()
-fps = 60
-
+# define dimensions
 screen_width = 1200
 screen_height = 800
-
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Worlds Hardest Game')
-
-# .Rect(x-coord, y-coord, width, height)
-player = pygame.Rect(50, 50, 30, 30)
-goal = pygame.Rect(1100, 50, 50, 50)
 
 # define common colors
 black = (0, 0, 0)
@@ -22,25 +11,14 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-# define font
-font = pygame.font.SysFont('Bauhaus 93', 60)
-
 # show text on screen (this probs is not the best way to do this...)
-def draw_text(text, font, text_col, x, y):
+def draw_text(text, font, text_col, x, y, screen):
 	img = font.render(text, True, text_col)
 	screen.blit(img, (x, y))
 
 # fun background for later
 # bg = pygame.image.load('img/space.jpg')
 
-walls = [
-pygame.Rect(0,0,20,screen_height),
-pygame.Rect(0,0,screen_width, 20),
-pygame.Rect(0, screen_height-20, screen_width, 20),
-pygame.Rect(screen_width-20, 0, 20, screen_height),
-pygame.Rect(screen_width/3, 0, 20, screen_height-200),
-pygame.Rect(screen_width * 2/3, 200, 20, screen_height - 200)
-]
 
 class Obstacle(pygame.Rect):
     def __init__(self, x, y, width, height, vel):
@@ -51,78 +29,107 @@ class Obstacle(pygame.Rect):
         if self.right > screen_width * 2/3 or self.left < screen_width/3 + 20:
             self.vel = -self.vel
 
-vel_left = 3
-vel_right = -3
+def main():
+    pygame.init()
 
-obstacles = [
-    Obstacle(screen_width/3 + 20, 240, 30, 30, vel_left),
-    Obstacle(screen_width/3 + 20, 360, 30, 30, vel_left),
-    Obstacle(screen_width/3 + 20, 480, 30, 30, vel_left),
-    Obstacle(screen_width * 2/3 - 50, 300, 30, 30, vel_right),
-    Obstacle(screen_width * 2/3 - 50, 420, 30, 30, vel_right),
-    Obstacle(screen_width * 2/3 - 50, 540, 30, 30, vel_right),
-]
+    clock = pygame.time.Clock()
+    fps = 60
 
-run = True
-alive = True
-win = False
-while run:
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption('Worlds Hardest Game')
 
-    clock.tick(fps)
+    walls = [
+        pygame.Rect(0,0,20,screen_height),
+        pygame.Rect(0,0,screen_width, 20),
+        pygame.Rect(0, screen_height-20, screen_width, 20),
+        pygame.Rect(screen_width-20, 0, 20, screen_height),
+        pygame.Rect(screen_width/3, 0, 20, screen_height-200),
+        pygame.Rect(screen_width * 2/3, 200, 20, screen_height - 200)
+    ]
+    
+    vel_left = 3
+    vel_right = -3
 
-    #draw background
-    # screen.blit(bg, (0,0))
+    obstacles = [
+        Obstacle(screen_width/3 + 20, 240, 30, 30, vel_left),
+        Obstacle(screen_width/3 + 20, 360, 30, 30, vel_left),
+        Obstacle(screen_width/3 + 20, 480, 30, 30, vel_left),
+        Obstacle(screen_width * 2/3 - 50, 300, 30, 30, vel_right),
+        Obstacle(screen_width * 2/3 - 50, 420, 30, 30, vel_right),
+        Obstacle(screen_width * 2/3 - 50, 540, 30, 30, vel_right),
+    ]
 
-    #delay start of game by 10ms
-    pygame.time.delay(10)
+    # define font
+    font = pygame.font.SysFont('Bauhaus 93', 60)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    # .Rect(x-coord, y-coord, width, height)
+    player = pygame.Rect(50, 50, 30, 30)
+    goal = pygame.Rect(1100, 50, 50, 50)
 
-    # move player using arrow keys
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player.x > 0:
-        player.x -= 5
-    if keys[pygame.K_RIGHT] and player.x < screen_width - 50:
-        player.x += 5
-    if keys[pygame.K_UP] and player.y > 0:
-        player.y -= 5
-    if keys[pygame.K_DOWN] and player.y < screen_height - 50:
-        player.y += 5
+    run = True
+    alive = True
+    win = False
 
-    # collision with obstacle
-    for obstacle in obstacles:
-        obstacle.update()
-        if player.colliderect(obstacle):
-            alive = False
+    while run:
 
-    # collision with wall
-    for wall in walls:
-        if player.colliderect(wall):
-            alive = False
+        clock.tick(fps)
 
-    if player.colliderect(goal):
-        win = True
+        #draw background
+        # screen.blit(bg, (0,0))
 
-    screen.fill(black)
+        #delay start of game by 10ms
+        pygame.time.delay(10)
 
-    if alive == True and win == False:
-        pygame.draw.rect(screen, red, player)
-        pygame.draw.rect(screen, blue, goal)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-        for wall in walls:
-            pygame.draw.rect(screen, white, wall)
+        # move player using arrow keys
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x > 0:
+            player.x -= 5
+        if keys[pygame.K_RIGHT] and player.x < screen_width - 50:
+            player.x += 5
+        if keys[pygame.K_UP] and player.y > 0:
+            player.y -= 5
+        if keys[pygame.K_DOWN] and player.y < screen_height - 50:
+            player.y += 5
 
+        # collision with obstacle
         for obstacle in obstacles:
-            pygame.draw.rect(screen, green, obstacle)
+            obstacle.update()
+            if player.colliderect(obstacle):
+                alive = False
 
-    else:
-        if win:
-            draw_text("You Win!", font, white, 500, 20)
+        # collision with wall
+        for wall in walls:
+            if player.colliderect(wall):
+                alive = False
+
+        if player.colliderect(goal):
+            win = True
+
+        screen.fill(black)
+
+        if alive == True and win == False:
+            pygame.draw.rect(screen, red, player)
+            pygame.draw.rect(screen, blue, goal)
+
+            for wall in walls:
+                pygame.draw.rect(screen, white, wall)
+
+            for obstacle in obstacles:
+                pygame.draw.rect(screen, green, obstacle)
+
         else:
-            draw_text("Game over", font, white, 500, 20)
+            if win:
+                draw_text("You Win!", font, white, 500, 20, screen)
+            else:
+                draw_text("Game Over", font, white, 500, 20, screen)
 
-    pygame.display.update()
+        pygame.display.update()
 
-pygame.quit()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
