7ba70e203005dbd7bcc4cb9a8551196f58398693
cindyliu6 <69543494+cindyliu6@users.noreply.github.com>
2020-12-05 11:10:22 -0700

    updated player version
    walls update and collision check

--- Game Test/game.py
+++ Game Test/game.py
@@ -29,256 +29,277 @@ def draw_text(text, font, text_col, x, y, screen):
 # bg = pygame.image.load('img/space.jpg')
 
 DIR = {
-    'u' : (0, -1), # north is -y
-    'd' : (0, 1),
-    'l' : (-1,0),
-    'r' : (1,0)
-    }
+	'u' : (0, -1), # north is -y
+	'd' : (0, 1),
+	'l' : (-1,0),
+	'r' : (1,0)
+	}
 
 # drawing game surface
 def draw_grid(surface, walls):
-    for y in range(0, HEIGHT):
-        for x in range(0, WIDTH):
-            r = pygame.Rect((x * SIZE, y * SIZE), (SIZE, SIZE))
-            if (x, y) in walls:
-               color = (255,255,255)
-            else:
-               color = (0,0,0)
-            pygame.draw.rect(surface, color, r)
+	for y in range(0, HEIGHT):
+		for x in range(0, WIDTH):
+			r = pygame.Rect((x * SIZE, y * SIZE), (SIZE, SIZE))
+			if (x, y) in walls:
+			   color = (255,255,255)
+			else:
+			   color = (0,0,0)
+			pygame.draw.rect(surface, color, r)
 
 # moving obstacle class
 class Obstacle():
-    def __init__(self, x, y, vel):
-        self.vel = vel
-        self.position = (x, y)
+	def __init__(self, x, y, vel):
+		self.vel = vel
+		self.position = (x, y)
 
-    def update(self):
-        self.position = tuple(map(operator.add, self.position, (self.vel, 0)))
-        if self.position[0] > 40 or self.position[0] < 20:
-            self.vel = -self.vel
+	def update(self):
+		self.position = tuple(map(operator.add, self.position, (self.vel, 0)))
+		if self.position[0] > 40 or self.position[0] < 20:
+			self.vel = -self.vel
 
-    def draw(self, surface):
-        r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
-        pygame.draw.rect(surface, green, r)
+	def draw(self, surface):
+		r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
+		pygame.draw.rect(surface, green, r)
 
-    def get_position(self):
-        return self.position
+	def get_position(self):
+		return self.position
 
 # player class
 class Player():
 
-    def __init__(self, x, y):
-        self.position = (x, y)
-        pass
+	def __init__(self, x, y):
+		self.position = (x, y)
+		pass
 
-    def get_position(self):
-        return self.position
+	def get_position(self):
+		return self.position
 
-    def set_position(self, x, y):
-        self.position = (x, y)
+	def set_position(self, x, y):
+		self.position = (x, y)
 
-    def move(self, dir):
-        self.position = tuple(map(operator.add, self.position, DIR[dir]))
+	def move(self, dir):
+		self.position = tuple(map(operator.add, self.position, DIR[dir]))
 
-    def draw(self, surface):
-        r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
-        pygame.draw.rect(surface, red, r)
+	def draw(self, surface):
+		r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
+		pygame.draw.rect(surface, red, r)
 
 # goal class
 class Goal():
-    def __init__ (self, x, y):
-        self.position = (x, y)
+	def __init__ (self, x, y):
+		self.position = (x, y)
 
-    def draw(self, surface):
-        r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
-        pygame.draw.rect(surface, blue, r)
+	def draw(self, surface):
+		r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
+		pygame.draw.rect(surface, blue, r)
 
-    def get_position(self):
-        return self.position
+	def get_position(self):
+		return self.position
 
 # ai agent class
 class Agent():
-    def __init__(self, x, y):
-        self.player = Player(x,y);
+	def __init__(self, x, y):
+		self.player = Player(x,y);
 
-    def move(self):
-        i = random.randint(0, 4)
-        if i == 0:
-            self.player.move('l')
-        elif i == 1:
-            self.player.move('r')
-        elif i == 2:
-            self.player.move('u')
-        else:
-            self.player.move('d')
+	def move(self):
+		i = random.randint(0, 4)
+		if i == 0:
+			self.player.move('l')
+		elif i == 1:
+			self.player.move('r')
+		elif i == 2:
+			self.player.move('u')
+		else:
+			self.player.move('d')
 
-    def get_position(self):
-        return self.player.get_position()
+	def get_position(self):
+		return self.player.get_position()
 
-    def draw(self, surface):
-        self.player.draw(surface)
+	def draw(self, surface):
+		self.player.draw(surface)
+
 
 
 def main():
-    pygame.init()
+	pygame.init()
 
-    clock = pygame.time.Clock()
-    fps = 20
+	clock = pygame.time.Clock()
+	fps = 20
 
-    walls = []
+	walls = []
 
-    for x in range(WIDTH):
-        walls.append((x, 0))
-        walls.append((x, HEIGHT-1))
+	for x in range(WIDTH):
+		walls.append((x, 0))
+		walls.append((x, HEIGHT-1))
 
-    for x in range(HEIGHT):
-        walls.append((0, x))
-        walls.append((WIDTH-1, x))
+	for x in range(HEIGHT):
+		walls.append((0, x))
+		walls.append((WIDTH-1, x))
 
-    for x in range(HEIGHT-10):
-        walls.append((18, x))
+	for x in range(HEIGHT-10):
+		walls.append((18, x))
 
-    for x in range(10, HEIGHT):
-        walls.append((42, x))
+	for x in range(10, HEIGHT):
+		walls.append((42, x))
 
-    screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
-    pygame.display.set_caption('Worlds Hardest Game')
+	screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+	pygame.display.set_caption('Worlds Hardest Game')
 
-    draw_grid(screen, walls)
+	draw_grid(screen, walls)
 
-    player = Agent(5,10)
-    goal = Goal(END[0], END[1])
+	player = Agent(5,10)
+	goal = Goal(END[0], END[1])
 
-    vel_left = 1
-    vel_right = -1
+	vel_left = 1
+	vel_right = -1
 
-    obstacles = [[
-            Obstacle(31, 11, vel_left),
-            Obstacle(32, 12, vel_left),
-            Obstacle(33, 13, vel_left),
-            Obstacle(34, 14, vel_right),
-            Obstacle(35, 15, vel_right),
-            Obstacle(36, 16, vel_right)
-        ],
-        [
-            (31, 11),
-            (32,12),
-            (33,13),
-            (34,14),
-            (35,15),
-            (36,16)
-            ]
-    ]
+	obstacles = [[
+			Obstacle(31, 11, vel_left),
+			Obstacle(32, 12, vel_left),
+			Obstacle(33, 13, vel_left),
+			Obstacle(34, 14, vel_right),
+			Obstacle(35, 15, vel_right),
+			Obstacle(36, 16, vel_right)
+		],
+		[
+			(31, 11),
+			(32,12),
+			(33,13),
+			(34,14),
+			(35,15),
+			(36,16)
+			]
+	]
 
-    # define font
-    font = pygame.font.SysFont('Bauhaus 93', 60)
-    homepage_font = pygame.font.SysFont('Bauhaus 93', 45)
+	# define font
+	font = pygame.font.SysFont('Bauhaus 93', 60)
+	homepage_font = pygame.font.SysFont('Bauhaus 93', 45)
 
-    run = True
-    alive = True
-    win = False
+	run = True
+	alive = True
+	win = False
 
-    population = Population(100, START[0], START[1], END[0], END[1], 1000)
-    
-    homepage = True
-    gamemode = 0
+	population = Population(100, START[0], START[1], END[0], END[1], 1000)
+
+	homepage = True
+	gamemode = 0
+
+	while homepage:
+		clock.tick(fps)
 
 
-    while homepage:
-        clock.tick(fps)
-        
-        
-        image = pygame.image.load('Game Test/homepage2.jpg')
-        pygame.draw.ellipse(image, red, (230, 265 + gamemode * 60, 20, 20))
-        draw_text("PLAY GAME", homepage_font, black, 280, 250, image)
-        draw_text("TRAIN COMPUTER", homepage_font, black, 280, 310, image)
-        draw_text("WATCH COMPUTER", homepage_font, black, 280, 370, image)
+		image = pygame.image.load('Game Test/homepage2.jpg')
+		pygame.draw.ellipse(image, red, (230, 265 + gamemode * 60, 20, 20))
+		draw_text("PLAY GAME", homepage_font, black, 280, 250, image)
+		draw_text("TRAIN COMPUTER", homepage_font, black, 280, 310, image)
+		draw_text("WATCH COMPUTER", homepage_font, black, 280, 370, image)
 
-        screen.blit(image, (0,0))
+		screen.blit(image, (0,0))
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            gamemode = (gamemode - 1) % 3
-        elif keys[pygame.K_DOWN]:
-            gamemode = (gamemode + 1) % 3
-        elif keys[pygame.K_RIGHT]:
-            homepage = False
-        
-        pygame.display.update()
-        print (gamemode)
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_UP]:
+			gamemode = (gamemode - 1) % 3
+		elif keys[pygame.K_DOWN]:
+			gamemode = (gamemode + 1) % 3
+		elif keys[pygame.K_RIGHT]:
+			homepage = False
 
-    if gamemode == 0:
-        player = Player (5,5)
+		pygame.display.update()
+		print (gamemode)
 
-        while run:
-            clock.tick(fps)
+	if gamemode == 0:
+		player = Player (5,5)
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
+		def collisions(solids, x, y):
+			for solid in solids:
+				if player.get_position()[0] == solid[0] + x and player.get_position()[1] == solid[1] + y:
+						print("collision")
+						return True
+			return False
 
-            draw_grid(screen, walls)
-            goal.draw(screen)
-            player.draw(screen)
+		while run:
+			clock.tick(fps)
 
-            for i in range(len(obstacles[0])):
-                obstacles[0][i].update()
-                obstacles[0][i].draw(screen)
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					run = False
 
-            # move player using arrow keys
-            move = pygame.key.get_pressed()
-            if move[pygame.K_LEFT] and player.get_position()[0] > 0:
-                player.move('l')
-            if move[pygame.K_RIGHT] and player.get_position()[0] < WIDTH - 1:
-                player.move('r')
-            if move[pygame.K_UP] and player.get_position()[1] > 0:
-                player.move('u')
-            if move[pygame.K_DOWN] and player.get_position()[1] < HEIGHT - 1:
-                player.move('d')
+			if alive == True and win == False:
+				print (player.get_position())
 
-            print (player.get_position())
+				draw_grid(screen, walls)
+				goal.draw(screen)
+				player.draw(screen)
 
-            pygame.display.update()
+				for i in range(len(obstacles[0])):
+					obstacles[0][i].update()
+					obstacles[0][i].draw(screen)
+					if player.get_position() == obstacles[0][i].get_position():
+						alive = False
 
-    elif gamemode == 1:
-        while run:
-            clock.tick(fps)
+				if player.get_position() == goal.get_position():
+					win = True
 
-            #delay start of game by 10ms
-            pygame.time.delay(10)
+			else:
+				if win:
+					draw_text("You Win!", font, white, 500, 20, screen)
+				else:
+					draw_text("Game Over", font, white, 500, 20, screen)
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
+			# move player using arrow keys
+			move = pygame.key.get_pressed()
+			if move[pygame.K_LEFT] and not collisions(walls, 1, 0):
+				player.move('l')
+			if move[pygame.K_RIGHT] and not collisions(walls, -1, 0):
+				player.move('r')
+			if move[pygame.K_UP] and not collisions(walls, 0, 1):
+				player.move('u')
+			if move[pygame.K_DOWN] and not collisions(walls, 0, -1):
+				player.move('d')
 
-            draw_grid(screen, walls)
-            goal.draw(screen)
+			pygame.display.update()
+			if not run:
+				pygame.quit()
 
-            for i in range(len(obstacles[0])):
-                obstacles[0][i].update()
-                obstacles[0][i].draw(screen)
-                obstacles[1][i] = obstacles[0][i].get_position()
+	elif gamemode == 1:
+		while run:
+			clock.tick(fps)
 
-            pygame.time.wait(1)
+			#delay start of game by 10ms
+			pygame.time.delay(10)
 
-            allDead = population.allDotsDead() 
-            if allDead:
-                population.calculateFitness()
-                population.naturalSelection()
-                population.mutateBabies()
-            else:
-                population.update(walls, obstacles[1])
-                population.show(screen)
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					run = False
 
-            #print("update")
-            pygame.display.update()
+			draw_grid(screen, walls)
+			goal.draw(screen)
 
-            if not run:
-                pygame.quit()
+			for i in range(len(obstacles[0])):
+				obstacles[0][i].update()
+				obstacles[0][i].draw(screen)
+				obstacles[1][i] = obstacles[0][i].get_position()
+
+			pygame.time.wait(1)
+
+			allDead = population.allDotsDead()
+			if allDead:
+				population.calculateFitness()
+				population.naturalSelection()
+				population.mutateBabies()
+			else:
+				population.update(walls, obstacles[1])
+				population.show(screen)
+
+			#print("update")
+			pygame.display.update()
+
+			if not run:
+				pygame.quit()
 
 if __name__ == "__main__":
-    main()
+	main()
