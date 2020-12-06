ac7c3b9248ebb5039d6f5bdc6699998ba63c5df8
joeyliu19 <70824234+joeyliu19@users.noreply.github.com>
2020-12-06 14:22:17 -0800

    obstacle generalized
    added bounds to obstacle class

---
+++ Game Test/joeygame.py
@@ -0,0 +1,311 @@
+import pygame
+import numpy as np
+import operator
+import random
+from Population import Population
+
+# define dimensions
+HEIGHT = 40
+WIDTH = 60
+SIZE = 15
+screen_width = WIDTH * SIZE
+screen_height = HEIGHT * SIZE
+START = (5, 5)
+END = (50, 35)
+
+# define common colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+# show text on screen (this probs is not the best way to do this...)
+def draw_text(text, font, text_col, x, y, screen):
+	img = font.render(text, True, text_col)
+	screen.blit(img, (x, y))
+
+# fun background for later
+# bg = pygame.image.load('img/space.jpg')
+
+DIR = {
+	'u' : (0, -1), # north is -y
+	'd' : (0, 1),
+	'l' : (-1,0),
+	'r' : (1,0)
+	}
+
+# drawing game surface
+def draw_grid(surface, walls):
+	for y in range(0, HEIGHT):
+		for x in range(0, WIDTH):
+			r = pygame.Rect((x * SIZE, y * SIZE), (SIZE, SIZE))
+			if (x, y) in walls:
+			   color = (255,255,255)
+			else:
+			   color = (0,0,0)
+			pygame.draw.rect(surface, color, r)
+
+# moving obstacle class
+class Obstacle():
+	def __init__(self, x, y, vel, x_bound, x_bound2, y_bound, y_bound2):
+		self.vel = vel
+		self.position = (x, y)
+		self.xlimit = (x_bound)
+		self.xlimit2 = (x_bound2)
+		self.ylimit = (y_bound)
+		self.ylimit2 = (y_bound2)
+
+	def update(self):
+		self.position = tuple(map(operator.add, self.position, (self.vel, 0)))
+		if self.position[0] > self.xlimit or self.position[0] < self.xlimit2:
+			self.vel = -self.vel
+		if self.position[1] > self.ylimit or self.position[1] < self.ylimit2:
+			self.vel = -self.vel
+
+	def draw(self, surface):
+		r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
+		pygame.draw.rect(surface, green, r)
+
+	def get_position(self):
+		return self.position
+
+# player class
+class Player():
+
+	def __init__(self, x, y):
+		self.position = (x, y)
+		pass
+
+	def get_position(self):
+		return self.position
+
+	def set_position(self, x, y):
+		self.position = (x, y)
+
+	def move(self, dir):
+		self.position = tuple(map(operator.add, self.position, DIR[dir]))
+
+	def draw(self, surface):
+		r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
+		pygame.draw.rect(surface, red, r)
+
+# goal class
+class Goal():
+	def __init__ (self, x, y):
+		self.position = (x, y)
+
+	def draw(self, surface):
+		r = pygame.Rect((self.position[0]*SIZE,self.position[1]*SIZE), (SIZE, SIZE))
+		pygame.draw.rect(surface, blue, r)
+
+	def get_position(self):
+		return self.position
+
+# ai agent class
+class Agent():
+	def __init__(self, x, y):
+		self.player = Player(x,y);
+
+	def move(self):
+		i = random.randint(0, 4)
+		if i == 0:
+			self.player.move('l')
+		elif i == 1:
+			self.player.move('r')
+		elif i == 2:
+			self.player.move('u')
+		else:
+			self.player.move('d')
+
+	def get_position(self):
+		return self.player.get_position()
+
+	def draw(self, surface):
+		self.player.draw(surface)
+
+
+
+def main():
+	pygame.init()
+
+	clock = pygame.time.Clock()
+	fps = 20
+
+	walls = []
+
+	for x in range(WIDTH):
+		walls.append((x, 0))
+		walls.append((x, HEIGHT-1))
+
+	for x in range(HEIGHT):
+		walls.append((0, x))
+		walls.append((WIDTH-1, x))
+
+	for x in range(HEIGHT-10):
+		walls.append((18, x))
+
+	for x in range(10, HEIGHT):
+		walls.append((42, x))
+
+	screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+	pygame.display.set_caption('Worlds Hardest Game')
+
+	draw_grid(screen, walls)
+
+	player = Agent(5,10)
+	goal = Goal(END[0], END[1])
+
+	vel_left = 1
+	vel_right = -1
+
+	obstacles = [[
+			Obstacle(31, 11, vel_left, 40, 20, 10000, -100000),
+			Obstacle(32, 12, vel_left, 40, 20, 10000, -10000),
+			Obstacle(33, 13, vel_left, 40, 20, 10000, -10000),
+			Obstacle(34, 14, vel_right, 40, 20, 10000, -100000),
+			Obstacle(35, 15, vel_right, 40, 20, 10000, -10000),
+			Obstacle(36, 16, vel_right, 40, 20, 10000, -10000)
+		],
+		[
+			(31, 11),
+			(32,12),
+			(33,13),
+			(34,14),
+			(35,15),
+			(36,16)
+			]
+	]
+
+	# define font
+	font = pygame.font.SysFont('Bauhaus 93', 60)
+	homepage_font = pygame.font.SysFont('Bauhaus 93', 45)
+
+	run = True
+	alive = True
+	win = False
+
+	population = Population(100, START[0], START[1], END[0], END[1], 1000)
+
+	homepage = True
+	gamemode = 0
+
+	while homepage:
+		clock.tick(fps)
+
+
+		image = pygame.image.load('Game Test/homepage2.jpg')
+		pygame.draw.ellipse(image, red, (230, 265 + gamemode * 60, 20, 20))
+		draw_text("PLAY GAME", homepage_font, black, 280, 250, image)
+		draw_text("TRAIN COMPUTER", homepage_font, black, 280, 310, image)
+		draw_text("WATCH COMPUTER", homepage_font, black, 280, 370, image)
+
+		screen.blit(image, (0,0))
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_UP]:
+			gamemode = (gamemode - 1) % 3
+		elif keys[pygame.K_DOWN]:
+			gamemode = (gamemode + 1) % 3
+		elif keys[pygame.K_RIGHT]:
+			homepage = False
+
+		pygame.display.update()
+		# print (gamemode)
+
+	if gamemode == 0:
+		player = Player (5,5)
+
+		def collisions(solids, x, y):
+			for solid in solids:
+				if player.get_position()[0] == solid[0] + x and player.get_position()[1] == solid[1] + y:
+						# print("collision")
+						return True
+			return False
+
+		while run:
+			clock.tick(fps)
+
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					run = False
+
+			if alive == True and win == False:
+				# print (player.get_position())
+
+				draw_grid(screen, walls)
+				goal.draw(screen)
+				player.draw(screen)
+
+				for i in range(len(obstacles[0])):
+					obstacles[0][i].update()
+					obstacles[0][i].draw(screen)
+					if player.get_position() == obstacles[0][i].get_position():
+						alive = False
+
+				if player.get_position() == goal.get_position():
+					win = True
+
+			else:
+				if win:
+					draw_text("You Win!", font, white, 500, 20, screen)
+				else:
+					draw_text("Game Over", font, white, 500, 20, screen)
+
+			# move player using arrow keys
+			move = pygame.key.get_pressed()
+			if move[pygame.K_LEFT] and not collisions(walls, 1, 0):
+				player.move('l')
+			if move[pygame.K_RIGHT] and not collisions(walls, -1, 0):
+				player.move('r')
+			if move[pygame.K_UP] and not collisions(walls, 0, 1):
+				player.move('u')
+			if move[pygame.K_DOWN] and not collisions(walls, 0, -1):
+				player.move('d')
+
+			pygame.display.update()
+			if not run:
+				pygame.quit()
+
+	elif gamemode == 1:
+		while run:
+			clock.tick(fps)
+
+			#delay start of game by 10ms
+			pygame.time.delay(10)
+
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					run = False
+
+			draw_grid(screen, walls)
+			goal.draw(screen)
+
+			for i in range(len(obstacles[0])):
+				obstacles[0][i].update()
+				obstacles[0][i].draw(screen)
+				obstacles[1][i] = obstacles[0][i].get_position()
+
+			pygame.time.wait(1)
+
+			allDead = population.allDotsDead()
+			if allDead:
+				population.calculateFitness()
+				population.naturalSelection()
+				population.mutateBabies()
+			else:
+				population.update(walls, obstacles[1])
+				population.show(screen)
+
+			#print("update")
+			pygame.display.update()
+
+			if not run:
+				pygame.quit()
+
+if __name__ == "__main__":
+	main()
