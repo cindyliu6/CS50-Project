62d95ca1ecc6765493c2b816fc22f012fde350f2
joeyliu19 <70824234+joeyliu19@users.noreply.github.com>
2020-12-07 11:13:53 -0800

    copy and pasted joeygame into level2, the obstacle class is slightly modified to make obstacles move up and down too.

--- Game Test/level2.py
+++ Game Test/level2.py
@@ -153,20 +153,51 @@ def main():
 	vel_down = 1
 
 	obstacles = [[
-			Obstacle(31, 11, vel_left, 0, 40, 20, 10000, -100000),
-			Obstacle(32, 12, vel_left, 0, 40, 20, 10000, -100000),
-			Obstacle(33, 13, vel_left, 0, 40, 20, 10000, -100000),
-			Obstacle(34, 14, vel_right, 0, 40, 20, 10000, -100000),
-			Obstacle(35, 15, vel_right, 0, 40, 20, 10000, -100000),
-			Obstacle(36, 16, vel_right, 0, 40, 20, 10000, -100000),
+			Obstacle(20, 9, 0, vel_down, 40, 20, 30, 9),
+			Obstacle(21, 9, 0, vel_down, 40, 20, 30, 9),
+			Obstacle(22, 9, 0, vel_down, 40, 20, 30, 9),
+			Obstacle(23, 9, 0, vel_down, 40, 20, 30, 9),
+			Obstacle(24, 9, 0, vel_down, 40, 20, 30, 9),
+			Obstacle(25, 9, 0, vel_down, 40, 20, 30, 9),
+            Obstacle(26, 9, 0, vel_down, 40, 20, 30, 9),
+            Obstacle(27, 9, 0, vel_down, 40, 20, 30, 9),
+            Obstacle(28, 9, 0, vel_down, 40, 20, 30, 9),
+            Obstacle(29, 9, 0, vel_down, 40, 20, 30, 9),
+            Obstacle(30, 9, 0, vel_down, 40, 20, 30, 9),
+            Obstacle(31, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(32, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(33, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(34, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(35, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(36, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(37, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(38, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(39, 30, 0, vel_up, 40, 20, 30, 9),
+            Obstacle(40, 30, 0, vel_up, 40, 20, 30, 9)
 		],
 		[
-			(31, 11),
-			(32, 12),
-			(33, 13),
-			(34, 14),
-			(35, 15),
-			(36, 16)
+			(20, 9),
+			(21, 9),
+			(22, 9),
+			(23, 9),
+			(24, 9),
+			(25, 9),
+            (26, 9),
+            (27, 9),
+            (28, 9),
+            (29, 9),
+            (30, 9),
+            (31, 30),
+            (32, 30),
+            (33, 30),
+            (34, 30),
+            (35, 30),
+            (36, 30),
+            (37, 30),
+            (38, 30),
+            (39, 30),
+            (40, 30)
+
 			]
 	]
 
